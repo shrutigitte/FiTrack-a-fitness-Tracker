a25dcf6844cf78e77d5e6dba2c969ca979f25c0c
Shruti Gitte <shrutigitte@gmail.com>
2024-07-29 16:51:12 +0530

    Already entered data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,17 +29,9 @@ export class AppComponent implements OnInit {
   selectedType: string = '';
   workoutTypes: string[] = ['Running', 'Cycling', 'Skipping', 'Pilates'];
 
-  workouts: Workout[] = [
-    { name: 'Kriti Mahajan', type: 'Running', minutes: 30 },
-    { name: 'Vedica Jhalani', type: 'Cycling', minutes: 45 },
-    { name: 'Aashi Yadav', type: 'Swimming', minutes: 60 },
-    { name: 'Sarthak Kulkarni', type: 'Running', minutes: 20 },
-    { name: 'Harry Potter', type: 'Running', minutes: 10 },
-    { name: 'Nikitha Gowda', type: 'Yoga', minutes: 50 },
-    { name: 'Milani Gaur', type: 'Cycling', minutes: 40 },
-    { name: 'Sunanda Kendre', type: 'Skipping', minutes: 40 },
-    { name: 'Prajakta Shikhre', type: 'Pilates', minutes: 40 }
-  ];
+  workouts: { name: string, type: string, minutes: number }[] = [];
+
+  
   displayedWorkouts: Workout[] = [];
   pageSize = 5;
   pageIndex = 1;
--- src/app/workout.service.ts
+++ src/app/workout.service.ts
@@ -5,8 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class WorkoutService {
   private workouts = [
-    { name: 'John Doe', type: 'Running', minutes: 30 },
-    { name: 'Jane Smith', type: 'Swimming', minutes: 45 }
+    { name: 'Kriti Mahajan', type: 'Running', minutes: 30 },
+    { name: 'Vedica Jhalani', type: 'Cycling', minutes: 45 },
+    { name: 'Aashi Yadav', type: 'Swimming', minutes: 60 },
+    { name: 'Sarthak Kulkarni', type: 'Running', minutes: 20 },
+    { name: 'Harry Potter', type: 'Running', minutes: 10 },
+    { name: 'Nikitha Gowda', type: 'Yoga', minutes: 50 },
+    { name: 'Milani Gaur', type: 'Cycling', minutes: 40 },
+    { name: 'Sunanda Kendre', type: 'Skipping', minutes: 40 },
+    { name: 'Prajakta Shikhre', type: 'Pilates', minutes: 40 }
   ];
 
   getWorkouts() {
